0bdf5e882c737c0cd98f141c175af2388a517105
Mohankumar Saraswathy <142045407+mohankumarsaras@users.noreply.github.com>
2026-07-28 00:39:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [react()],
-    base: env.BASE_PATH || '/',
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
 })
